b7ec5ab830f76adeeedab6d4b2c2d4cd2ba6c1ec
Mickaël Lacombe <mickael@mbp-de-mickael.home>
2018-02-21 17:54:08 +0100

    hide conf

---
+++ src/config_copy.js
@@ -0,0 +1,8 @@
+const config = {};
+
+config.client_id = 'HERE';
+config.client_secret = 'HERE';
+config.ipProxyCors = 'IP adresse';
+config.redirect_uri = 'http://localhost:3000/auth/callback'
+
+export default config
